bf1814e3aa8e0b330c01f78eb52264e7f4a4f6a7
Reece Hebel <reecehebel@yahoo.com>
2016-11-29 12:56:13 -0600

    Add project files.

---
+++ TheFSharpExperience/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace TheFSharpExperience.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("TheFSharpExperience")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Hewlett-Packard Company")>]
+[<assembly: AssemblyProduct("TheFSharpExperience")>]
+[<assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9e985626-d4a6-4a81-9f55-4520adb5a41d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
